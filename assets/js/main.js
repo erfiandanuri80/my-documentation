befd80182472c67bacda0c13116712271105e335
Rio Erfian 180411100040 <48340965+erfiandanuri80@users.noreply.github.com>
2020-04-09 12:21:55 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,24 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
+// ========================================================================= //
+  //  Typed Js
+  // ========================================================================= //
+
+  var typed = $(".typed");
+
+  $(function() {
+    var strings = $('.typed-items').text();
+    strings = $('.typed-items').data('typed-person') + ',' + strings;
+    strings = strings.split(',');
+
+    typed.typed({
+      strings: strings,
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+  
 $(function() {
 
 	// Vars.
